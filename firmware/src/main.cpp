1099a69e3a4822fbd3322360f528854c37739f53
Marcus Roth <168829014+M-C-machts@users.noreply.github.com>
2025-07-27 22:00:17 +0200

    Add first working implementation

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,48 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <string>
+
+#include <Arduino.h>
+#include <Adafruit_INA228.h>
+#include <Wire.h>
+
+Adafruit_INA228 ina;
+const uint8_t LED_PIN = 25;   // on‑board LED
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+	delay(500);
+
+    Wire.setClock(400000);
+	Wire.begin();
+
+    if (!ina.begin(0x40)) {
+        Serial.println("INA228 not found!");
+        while (true) { 
+			digitalWrite(LED_PIN, HIGH);
+			Serial.println("INA228 not found!");
+			delay(200); 
+			digitalWrite(LED_PIN, LOW); 
+			delay(200); }
+    }
+
+    ina.setShunt(0.015);   // 15 mΩ external shunt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    float busVoltage 	= ina.readBusVoltage();	// [V]
+    // float shuntVoltage 	= ina.readShuntVoltage();
+    float current 		= ina.readCurrent();	// [mA]
+    float power   		= ina.readPower();
+	float resistance	= busVoltage / current;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	
+
+	char temp[64];
+	sprintf(temp, "%.2f,%.3f,%.3f,%.3f", busVoltage, current, power, resistance);
+	std::string buffer = temp;
+	Serial.println(buffer.c_str());
+
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(400);
 }
